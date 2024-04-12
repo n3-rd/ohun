51d8ae81fed2e996dfeaef0dec9c4fade0e28f2d
N3-rd <godwinjdayo@gmail.com>
2024-04-12 12:42:12 +0100

    Initial commit

---
+++ src/lib/components/BackButton.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { ArrowLeft } from 'lucide-svelte';
+
+	export const goBack = () => {
+		window.history.back();
+	};
+</script>
+
+<button
+	class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-[#ebebeb] hover:bg-gray-300"
+	on:click={() => goBack()}
+>
+	<ArrowLeft size="15" stroke-width="3" />
+</button>
---
+++ src/lib/components/Titlebar.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { windowMaximized } from '$lib/stores/window-store';
+
+	import { appWindow } from '@tauri-apps/api/window';
+	import { Maximize2, Minimize2, Minus, X } from 'lucide-svelte';
+
+	export let title = 'Stauri';
+
+	const minimizeWindow = async () => {
+		await appWindow.minimize();
+	};
+
+	const maximizeWindow = async () => {
+		await appWindow.toggleMaximize();
+		windowMaximized.update((value) => !value);
+	};
+
+	const closeWindow = async () => {
+		await appWindow.close();
+	};
+</script>
+
+<div
+	data-tauri-drag-region
+	class="z-100 flex w-screen select-none justify-between rounded-t-xl bg-transparent px-2 text-gray-700"
+>
+	<div class="app-title cursor-default p-2 font-semibold" data-tauri-drag-region>
+		{title}
+	</div>
+
+	<ul class="flex items-center gap-3">
+		<button
+			class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-[#ebebeb] hover:bg-gray-300"
+			on:click={minimizeWindow}
+		>
+			<Minus size="15" stroke-width="3" />
+		</button>
+		<button
+			class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-[#ebebeb] hover:bg-gray-300"
+			on:click={maximizeWindow}
+		>
+			{#if $windowMaximized}
+				<Minimize2 size="15" stroke-width="3" />
+			{:else}
+				<Maximize2 size="15" stroke-width="3" />
+			{/if}
+		</button>
+		<button
+			class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-[#ebebeb] hover:bg-gray-300"
+			on:click={closeWindow}
+		>
+			<X size="15" stroke-width="3" />
+		</button>
+	</ul>
+</div>
