91114686291b40c7bc639d63342abe65c36fb951
n3-rd <godwinjdayo@gmail.com>
2024-04-18 12:15:01 +0100

    scroll to line

--- src/lib/components/Lyrics.svelte
+++ src/lib/components/Lyrics.svelte
@@ -3,6 +3,7 @@
 	import { copyText } from 'svelte-copy';
 	import { toast } from 'svelte-sonner';
 	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+	import { onMount } from 'svelte';
 
 	export const copy = (text: string) => {
 		copyText(text);
@@ -14,6 +15,13 @@
 			},
 			position: 'top-right'
 		});
+	};
+
+	const scrollTo = (index) => {
+		const element = document.getElementById(index);
+		if (element) {
+			element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+		}
 	};
 
 	let lyricWithIndex;
@@ -38,15 +46,18 @@
 	}
 
 	$: {
-		// console.log($syncedLyrics);
-		// console.log($plainLyrics);
 		lyricWithIndex = $plainLyrics.split('\n').map((line, index) => ({ text: line, index }));
-		console.log('lyrics', lyrics);
-		console.log('current', $currentLine);
-		// console.log($currentLine);
 	}
 
-	$: lines = $plainLyrics.split('\n');
+	setInterval(() => {
+		// const currentTime = Math.floor($currentLine.time);
+		console.log('scrolling to current line', $currentLine.time);
+		scrollTo($currentLine.time);
+	}, 1000);
+
+	onMount(() => {
+		scrollTo(0);
+	});
 </script>
 
 <div class="flex h-[90vh] min-w-full items-center justify-center px-4">
@@ -54,20 +65,16 @@
 	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 	<ScrollArea
 		class="sm:text-1xl h-full cursor-copy whitespace-pre-wrap
-	  text-center text-2xl font-extrabold !leading-[4.25rem] md:text-3xl xl:text-6xl"
-		on:click={() => copyText($currentLine.text)}
+	  text-center text-2xl font-extrabold leading-[4.25rem] md:text-3xl md:leading-[5.25rem] xl:text-6xl xl:leading-[6.25rem]"
 	>
-		{#each lyrics as line}
-			<p class="opacity-60" style={line.time == $currentLine.time ? 'opacity: 1' : 'opacity: 0.6'}>
+		{#each lyrics as line, i (i)}
+			<p
+				class="opacity-60 line-{i}"
+				style={line.time == $currentLine.time ? 'opacity: 1' : 'opacity: 0.6'}
+				id={line.time}
+			>
 				{line.text}
 			</p>
 		{/each}
 	</ScrollArea>
 </div>
-
-<style>
-	.current-line {
-		color: red;
-		font-weight: bold;
-	}
-</style>
