6c1d296c015f7a32ef60fa435661b3c06e5d4712
n3-rd <godwinjdayo@gmail.com>
2025-02-03 00:24:53 +0100

    album art cache fix

--- src/lib/stores/player-store.ts
+++ src/lib/stores/player-store.ts
@@ -1,5 +1,6 @@
 import type { Song } from "$lib/types";
 import { writable } from "svelte/store";
+import { persisted } from 'svelte-persisted-store';
 
 export const currentPlayingSong = writable<Song>({
     artist: null,
@@ -7,8 +8,14 @@ export const currentPlayingSong = writable<Song>({
     album: null
 });
 
-export const playTime = writable<number>(0);
+export const cachedAlbumArt = persisted<Record<string, string>>('cachedAlbumArt', {}, {
+    storage: 'local',
+    syncTabs: true
+});
+
 export const albumArt = writable('');
 export const accentColor = writable('#ffffff');
 export const textColor = writable('#000000');
 export const isLoading = writable(false);
+export const playTime = writable(0);
+export const duration = writable(0);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Lyrics from '$lib/components/Lyrics.svelte';
+	import { playTime, duration } from '$lib/stores/player-store';
 </script>
 
 <Lyrics />
